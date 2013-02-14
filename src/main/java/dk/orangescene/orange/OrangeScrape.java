d7df0788b35912c5bd7667d10aa83b2aeb11fe45
Johan Thomsen <jot@colabo.dk>
2013-02-15 00:31:44 +0100

    Fix: cannot find module-dropdown

--- src/main/java/dk/orangescene/orange/OrangeScrape.java
+++ src/main/java/dk/orangescene/orange/OrangeScrape.java
@@ -72,7 +72,7 @@ public class OrangeScrape {
     private void runImport() {
         System.out.println("Importerer RFMDB dump");
 
-        Select s = new Select(driver.findElement(new By.ByName("SET[function]")));
+        Select s = new Select(driver.findElement(new By.ByXPath("//select")));
         s.selectByVisibleText("Importer downloaded dump");
         selenium.waitForPageToLoad("5000");
         driver.findElement(new By.ByXPath("//input[@value='Importer data']")).click();
@@ -86,7 +86,7 @@ public class OrangeScrape {
 
         selenium.selectFrame("content");
 
-        Select s = new Select(driver.findElement(new By.ByName("SET[function]")));
+        Select s = new Select(driver.findElement(new By.ByXPath("//select")));
         s.selectByVisibleText("Download RFMDB dump");
         selenium.waitForPageToLoad("5000");
         driver.findElement(new By.ByXPath("//input[@value='Download RFMDB data']")).click();
